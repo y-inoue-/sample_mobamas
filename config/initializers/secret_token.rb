88eb3adf77fe3455d86bf312e5c6c58f7a7aeced
Yoshitaka Inoue <y-inoue@nbsi.jp>
2013-01-11 07:50:10 +0000

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleMobamas::Application.config.secret_token = 'aba96ef1ee4d97e94502a6df7cdc8fa0c47dc12b18a650f51e1f5af13b6f83e096ced0261b3253710b30771744135a26bf8f641ca4d34de72a24fd5f4303ec38'
