4f5c8b82ec14a45ff82b79330523d5ae4f03e550
Yoshitaka Inoue <y-inoue@nbsi.jp>
2013-01-17 06:42:37 +0000

    不要コード削除

--- app/controllers/my_studio_controller.rb
+++ app/controllers/my_studio_controller.rb
@@ -4,16 +4,4 @@ class MyStudioController < ApplicationController
     @user = current_user
   end
 
-  def cheer
-    user = current_user
-    @cheer_msg = ""
-    if user.cheer_point < 10 then
-      user.cheer_point += 1
-      user.save
-      @cheer_msg = "応援しました。友情pt:#{user.cheer_point}"
-    else
-      @cheer_msg = "これ以上は友情ptを増やせません"
-    end
-  end
-
 end
